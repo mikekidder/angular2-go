e9ae49074773e19baac8de43b0c1a69ec586bbaf
John Papa <john@johnpapa.net>
2016-01-17 08:34:12 -0500

    fixes

--- app/character.service.ts
+++ app/character.service.ts
@@ -1,31 +1,27 @@
 import { Injectable } from 'angular2/core';
-import { Http, Response } from 'angular2/http';
-import { Observable, Subscription } from 'rxjs';
+import { Http } from 'angular2/http';
 import 'rxjs/add/operator/map';
 
 import { Character } from './character.service';
+
+export interface Character {
+	id: number;
+	name: string;
+}
 
 @Injectable()
 export class CharacterService {
   constructor(private _http: Http) { }
 
   getCharacters() {
-    let observable = this._http.get('characters.json')
-      .map(function(response: Response) {
+    return this._http.get('characters.json')
+      .map(function(response) {
         return <Character[]>response.json();
       });
-
-    return observable;
   }
 
 	getCharacter(id: number) {
-    let observable = this._http.get('characters.json')
-      .map((response: Response) => <Character[]>response.json().filter((c: Character) => c.id === id)[0]);
-    return observable;
+    return this._http.get('characters.json')
+      .map((response) => <Character>response.json().filter((c: Character) => c.id === id)[0]);
 	}
-}
-
-export interface Character {
-	id: number;
-	name: string;
 }
